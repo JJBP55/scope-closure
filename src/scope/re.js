a981cdec8e084f55fb95ff1c76e1cafca3d4b40b
JJBP55 <jonathanjbello.jb@gmail.com>
2022-10-31 10:59:47 -0400

    reassignment and redeclaration

---
+++ src/scope/re.js
@@ -0,0 +1,29 @@
+var firstName; // Undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = 'David'; // declarar / asignar
+lastName = 'Ana';       // reasignando
+console.log(lastName);
+
+var secondName = 'David';// declarando / asignando
+var secondName = 'Ana';  // reasignando
+console.log(secondName);
+
+// Let
+
+let fruit = "Apple"; // declarar y asignar
+fruit = 'Kiwi'; // reasignando
+console.log(fruit);
+
+let fruit = 'Banana';
+console.log(fruit);
+// let no se puede redeclarar
+
+// Const
+
+const animal = 'dog'; // declarando y asginando
+// animal = 'cat'; // reasignando
+const animal = 'Snake';
+console.log(animal);
+
